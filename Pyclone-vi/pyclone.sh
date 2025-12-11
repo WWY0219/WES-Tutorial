59fe161869e37c9a88a6fc06b6e09960323e4126
WWY <173780817+WWY0219@users.noreply.github.com>
2025-12-11 20:41:06 +0800

    Update pyclone.sh

--- Pyclone-vi/pyclone.sh
+++ Pyclone-vi/pyclone.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+#SBATCH --job-name=wwy_pyclone
+#SBATCH --error=%x.%j.err
+#SBATCH --output=%x.%j.out
+#SBATCH --partition=cpu1,cpu2
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64000
+cd /data/person/g5/wangwy/ULM-Project/pyclone
+pyclone-vi fit -i US014-LM.tsv -o US014-LM.h5 -c 30 -d beta-binomial -r 10
+pyclone-vi write-results-file -i US014-LM.h5 -o US014-LM.tsv
